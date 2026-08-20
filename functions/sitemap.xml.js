dc63b9206db636bfde238d200bd23cee2016b884
K011Apse <he-god@outlook.com>
2026-08-20 17:33:48 +0800

    Add sitemap generation function

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,1 +1,19 @@
+// functions/sitemap.xml.js
+export function onRequest(context) {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://qrcode-converter.pages.dev/</loc>
+        <lastmod>2026-08-20</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>`;
 
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600'
+    }
+  });
+}
